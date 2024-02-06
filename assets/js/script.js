ecb503498e02830500ee9551cd56ea03b23d5d6d
John Lynch <johnf.lynch@yahoo.ie>
2024-02-06 17:36:23 +0000

    HTML and JS
    Fixed HTML typo addition missing
    
    JS
    Display addition question to user
    Alert/throw message if other types are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,10 +16,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit");
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("additon");
 });
 
 /**
@@ -27,11 +29,22 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame(){
+function runGame(gameType){
 
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    // Run gameType chosen by user
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+
+    // Else statement to alert developer of issue and "throw" show the gameType it occurred on.
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -51,7 +64,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
